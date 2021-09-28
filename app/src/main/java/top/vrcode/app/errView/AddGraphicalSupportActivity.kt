f2dc543864e1f4adb5395febf96ce3f6d752cbe5
Zxilly <zhouxinyu1001@gmail.com>
2021-09-28 20:19:07 +0800

    feat: add support for xwayland install

--- app/src/main/java/top/vrcode/app/errView/AddGraphicalSupportActivity.kt
+++ app/src/main/java/top/vrcode/app/errView/AddGraphicalSupportActivity.kt
@@ -2,11 +2,20 @@ package top.vrcode.app.errView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import top.vrcode.app.R
 
+// https://github.com/NeoTerm/NeoTerm/blob/236072395ce056d2d2cccf950d3f243f099a178f/app/src/main/java/io/neoterm/frontend/floating/dialog.kt
 class AddGraphicalSupportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_graphical_support)
+
+        val installBtn = findViewById<Button>(R.id.install_x_wayland)
+        installBtn.setOnClickListener {  }
+    }
+
+    fun installXWayland() {
+
     }
 }
